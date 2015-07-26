49b4a606911bed638070a9e76bcd560daf720135
李争献 <lizx3@ucweb.com>
2015-07-26 19:00:21 +0800

    保存与加载基本信息成功

--- src/edu/scut/se/lee/fragment/InputFragment.java
+++ src/edu/scut/se/lee/fragment/InputFragment.java
@@ -3,6 +3,8 @@ package edu.scut.se.lee.fragment;
 import net.tsz.afinal.annotation.view.ViewInject;
 import edu.scut.se.lee.R;
 import edut.scut.se.lee.util.Cache;
+import edut.scut.se.lee.util.Util;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -11,11 +13,21 @@ import android.widget.TextView;
 public class InputFragment extends BaseFragment implements OnClickListener {
 
 	@ViewInject(id = R.id.et_input_linelong)
-	private TextView et_linelong;
+	private TextView mEditLineLong;
+    private final String lineLong = "索长=";
 	@ViewInject(id = R.id.et_input_linename)
-	private TextView et_linename;
+	private TextView mEditLineName;
+    private final String lineName = "索名=";
+    @ViewInject(id=R.id.et_input_density)
+    private TextView mEditDensity;
+    private final String density = "密度=";
+    @ViewInject(id=R.id.et_input_strength)
+    private TextView mEditStrength;
+    private final String strength = "强度=";
 	@ViewInject(id = R.id.btn_input_save,click="onClick")
 	private Button btn_save;
+    @ViewInject(id=R.id.btn_input_load,click = "onClick")
+    private Button btn_load;
 
 	@Override
 	public int getRootRes() {
@@ -24,46 +36,44 @@ public class InputFragment extends BaseFragment implements OnClickListener {
 	}
 
 	@Override
-	public void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-	}
-
-	@Override
-	public void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-	}
-
-	@Override
 	public void initData() {
 		// TODO Auto-generated method stub
-		String s;
-		if (!(s = Cache.getInstance().load("linename", "null")).equals("null"))
-			et_linename.setText(s);
-		float f;
-		if ((f = Cache.getInstance().load("linelong", -1)) != -1) {
-			et_linelong.setText(f + "");
-		}
-	}
 
-	@Override
+    }
+
+    @Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case R.id.btn_input_save:
-			try {
-				Cache.getInstance().save(
-						"linelong",
-						Float.parseFloat(et_linelong.getText().toString()
-								.trim()));
-				Cache.getInstance().save("linename",
-						et_linename.getText().toString().trim());
-				showMsg("保存成功");
-			} catch (Exception e) {
-				showMsg("数据有误");
-			}
-			break;
-		}
+            case R.id.btn_input_save:
+                try {
+                    String content = lineName + mEditLineName.getText().toString().trim() + "\n";
+                    content += lineLong + mEditLineLong.getText().toString().trim() + "\n";
+                    content += density + mEditDensity.getText().toString().trim() + "\n";
+                    content += strength + mEditStrength.getText().toString().trim() + "\n";
+                    Util.saveFileInPrjDir("BaseInfo.txt", content);
+                    Util.showToast("保存基本信息为BaseInfo.txt成功");
+                } catch (Exception e) {
+                    Util.showToast("保存基本信息失败");
+                }
+                break;
+
+            case R.id.btn_input_load:
+                String[] strs = Util.loadFileLinesInPrjDir("BaseInfo.txt");
+                if (strs == null || strs.length != 4) {
+                    Util.showToast("没有已保存的数据");
+                    return;
+                }
+                String[] names = strs[0].split("=");
+                mEditLineName.setText(names.length == 2 ? names[1] : "0");
+                String[] longs = strs[1].split("=");
+                mEditLineLong.setText(longs.length == 2 ? longs[1] : "0");
+                String[] densities = strs[2].split("=");
+                mEditDensity.setText(densities.length == 2 ? densities[1] : "0");
+                String[] strengths = strs[3].split("=");
+                mEditStrength.setText(strengths.length == 2 ? strengths[1] : "0");
+                Util.showToast("数据加载完成");
+                break;
+        }
 	}
 }
--- src/edut/scut/se/lee/util/Util.java
+++ src/edut/scut/se/lee/util/Util.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.os.Environment;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.scut.se.lee.App;
 
@@ -17,5 +24,31 @@ public class Util {
 
     public static void showToast(String text) {
         Toast.makeText(App.getInstance(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void saveFileInPrjDir(String fileName,String content){
+        try {
+            FileWriter writer = new FileWriter(getPrjDir()+"/"+fileName);
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] loadFileLinesInPrjDir(String fileName){
+        try {
+            FileReader reader = new FileReader(getPrjDir()+"/"+fileName);
+            BufferedReader bReader = new BufferedReader(reader);
+            List<String> strs = new ArrayList<String>();
+            String str;
+            while(null != (str = bReader.readLine()))
+                strs.add(str);
+            bReader.close();
+            reader.close();
+            return strs.toArray(new String[strs.size()]);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
